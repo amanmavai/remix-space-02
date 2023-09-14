a20308066c2a258e60e1de7cad56fac5dcd39049
Aman Mavai <aman.mavai@gslab.com>
2023-09-14 11:55:24 +0530

    add tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -8,9 +8,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import tailwindStyles from "./styles/tailwind.css";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref ? [{rel: "stylesheet", href: cssBundleHref}] : [{rel: "stylesheet", href: tailwindStyles}]),
 ];
 
 export default function App() {
--- commands.md
+++ commands.md
@@ -1,9 +1,14 @@
 ```sh
 $ npx create-remix@latest
 
+# add prettier
 $ touch .prettierrc .prettierignore
-
 $ npm install -D prettier eslint-config-prettier
 
+# add tailwindcss
+# enable tailwind and postcss in remix.config.js
+$ npm install -D tailwindcss
+$ npx tailwindcss init --ts
+$ npm install -D autoprefixer postcss-import
 
 ```
