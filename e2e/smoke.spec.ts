f82c10a80ace9636645ab70d0f6bff44571e0e2a
Aman Mavai <aman.mavai@gslab.com>
2023-09-14 13:55:58 +0530

    improve e2e test

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -3,5 +3,6 @@ import {expect, test} from "@playwright/test";
 test("App loads and nav works", async ({page}) => {
   await page.goto("/");
 
-  // refer kentcdodds.com github to create this test
+  // refer https://github.com/kentcdodds/kentcdodds.com/blob/main/e2e/smoke.spec.ts
+  await expect(page.getByText(/Welcome to Remix/i)).toBeVisible();
 });
